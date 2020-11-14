53b03aa83f9e559c578f8785edfb258cef1d2fd2
Pavan Chilukuri <pavan.chilukuri@dhcs.ca.gov>
2020-11-13 16:22:47 -0800

    Add empty array utility

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ export const removeDuplicates = (array) => [...new Set(array)];
 
 export const generateAlphaNumericString = () =>
   Math.random().toString(36).slice(2);
+
+export const emptyArray = (array) => (array.length = 0);
